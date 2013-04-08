a329720290b686987a9c95b53564b36aa2a68e88
Arthur Yunusov <art.yunusov@gmail.com>
2013-04-08 21:21:51 +0600

    Update app.js

--- js/app/app.js
+++ js/app/app.js
@@ -1,10 +1,10 @@
 define(['backbone','app/router'],
   function(Backbone, Router){
     return {
-	  start: function(){
-	    this.router = new Router();
+      start: function(){
+        this.router = new Router();
         Backbone.history.start({pushState: true, hashChange: false, root: "/contacts/"});
-		Backbone.history.loadUrl("start");
+	Backbone.history.loadUrl("start");
       }
     };
   }
