cb0f5c8ecb8a1af9a16e1b22eb8f0e8eee78b634
Arthur Yunusov <art.yunusov@gmail.com>
2013-04-08 21:23:49 +0600

    Update add.js

--- js/controllers/add.js
+++ js/controllers/add.js
@@ -1,10 +1,10 @@
 define(['views/add', 'models/contact','app/global'],
   function(Add, Model, global){
     global.models.contact = new Model();
-	global.views.add = new Add(global.models.contact);
+    global.views.add = new Add(global.models.contact);
     return function() {
-	  global.models.contact.clear();
-	  global.views.add.render();
-  }
+      global.models.contact.clear();
+      global.views.add.render();
+    }
  }
 );
